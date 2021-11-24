45d93742d8d6ceaf1d7e28687f82bf8884cfbc06
Sacha Ponniah <sponniah812@gmail.com>
2021-11-24 15:28:52 +0000

    MVP complete.

---
+++ models/order_list.py
@@ -0,0 +1,6 @@
+from models.order import Order
+
+order_1 = Order("Amicus Apple", "24/11/21", 50, "limes")
+order_2 = Order("Wetherspoons", "24/11/21", 20, "1kg bags sawdust")
+orders = [order_1, order_2]
+
